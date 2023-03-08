4137401dab424f7a0a4f62f5b46688148c4dae22
Paulo Eduardo <pauloinfba@gmail.com>
2023-03-08 11:04:33 -0300

    Continuação do curso

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; // É preciso importar pq o TS não conseguiria encontrar
 
+/* Decorator do meu AppModule
+ * Existem 4 PROPRIEDADES sendo passadas
+ * bootstrap: responsável por dizer ao Angular
+ * "Qual componente você deve reconhecer no momento que o aplicativo inicia"
+ */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ServerComponent], // Aqui eu especifico que o ServerComponent existe. Apenas Criá-lo não é suficiente
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // Abaixo a configuração do meu DECORATOR, no caso @Component
+  selector: 'app-server', //Elemento unico, nao duplicado
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {}

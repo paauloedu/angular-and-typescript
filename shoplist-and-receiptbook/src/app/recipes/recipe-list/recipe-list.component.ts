c2a0b5944b66a4d467a53cfb223b3e9f64ace8cc
Paulo Eduardo <pauloinfba@gmail.com>
2023-03-22 07:29:36 -0300

    Criação de toda a estrutura da primeira parte

--- shoplist-and-receiptbook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoplist-and-receiptbook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'Uma Receita Teste',
+      'Apenas uma simples receita',
+      'https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg'
+    ),
+  ];
 }
--- shoplist-and-receiptbook/src/app/shopping-list/shopping-list.component.ts
+++ shoplist-and-receiptbook/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomate', 2),
+    new Ingredient('Cebola', 2),
+    new Ingredient('Calabresa', 1),
+    new Ingredient('Queijo', 1),
+  ];
 }

5cb273e80195eae0df2eebbf0b6d5ffc5d1ff907
Paulo Eduardo <pauloinfba@gmail.com>
2023-03-23 11:23:24 -0300

    Voltando a fazer o projeto com novos conhecimentos

--- shoplist-and-receiptbook/src/app/header/header.component.ts
+++ shoplist-and-receiptbook/src/app/header/header.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>(); // Crio meu evento para dizer qual foi a feature selecionada
+
+  // Nessa função eu recebo qual foi selecionada recipe ou shop e emito isso
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
+}

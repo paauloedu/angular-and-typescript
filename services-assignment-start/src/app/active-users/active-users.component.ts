4bffcbed6bec96b72feae6e4fe06bc35fe399d89
Paulo Eduardo <pauloinfba@gmail.com>
2023-03-28 11:23:56 -0300

    Implementando com services e contador init

--- services-assignment-start/src/app/active-users/active-users.component.ts
+++ services-assignment-start/src/app/active-users/active-users.component.ts
@@ -1,15 +1,22 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UsersService } from "../users.service";
 
 @Component({
-  selector: 'app-active-users',
-  templateUrl: './active-users.component.html',
-  styleUrls: ['./active-users.component.css']
+  selector: "app-active-users",
+  templateUrl: "./active-users.component.html",
+  styleUrls: ["./active-users.component.css"],
 })
-export class ActiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToInactive = new EventEmitter<number>();
+export class ActiveUsersComponent implements OnInit {
+  users: string[];
 
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    // Estamos dando um fetch para que sempre que for criar o component ActiveUsers, pegar os usuarios ativos.
+    // Isso é feito a partir do constructor que importa o userService e o ngOnit faz sempre essa inicialização.
+    this.users = this.userService.activeUsers;
+  }
   onSetToInactive(id: number) {
-    this.userSetToInactive.emit(id);
+    this.userService.setToInactive(id);
   }
 }
--- services-assignment-start/src/app/inactive-users/inactive-users.component.ts
+++ services-assignment-start/src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,23 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UsersService } from "../users.service";
 
 @Component({
-  selector: 'app-inactive-users',
-  templateUrl: './inactive-users.component.html',
-  styleUrls: ['./inactive-users.component.css']
+  selector: "app-inactive-users",
+  templateUrl: "./inactive-users.component.html",
+  styleUrls: ["./inactive-users.component.css"],
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+export class InactiveUsersComponent implements OnInit {
+  users: string[];
+
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    // Estamos dando um fetch para que sempre que for criar o component InactiveUsers, pegar os usuarios inativos.
+    // Isso é feito a partir do constructor que importa o userService e o ngOnit faz sempre essa inicialização.
+    this.users = this.userService.inactiveUsers;
+  }
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.userService.setToActive(id);
   }
 }

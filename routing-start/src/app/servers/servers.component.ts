65e626216e54b1f0558f65bf4a349e8ea9ae5a85
Paulo Eduardo <pauloinfba@gmail.com>
2023-03-30 11:17:03 -0300

    Trabalhando com roteamento

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  public servers: {id: number, name: string, status: string}[] = [];
+  public servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    // Pega a rota atual, um exemplo se tivessemos em local:4200/user/, pegaria esse path e nao o padrao local:4200
+    // this.router.navigate(['servers'], { relativeTo: this.route });
+  }
 }
--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+  user: { id: number; name: string };
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.user = {
+      // O snaphot pega uma imagem do momento,
+      id: this.route.snapshot.params['id'], //Pegamos o parametro que identificamos la nas rotas como :id
+      name: this.route.snapshot.params['name'],
+    };
+    //Agora, vamos ver o caso quando a pagina atualiza uma informação.
+    //Como meu metodo diz, eu sobrescrevo meus parametros, agora sim tudo certo.
+    this.route.params.subscribe((params: Params) => {
+      this.user.id = params['id'];
+      this.user.name = params['name'];
+      //Consigo reagir e atualizar as mudanças se estiver na msm pg, usando observáveis
+    });
   }
-
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
 }

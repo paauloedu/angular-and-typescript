eed1f9469f485e1fa40499ee1209bfcca88c75b5
Paulo Eduardo <pauloinfba@gmail.com>
2023-03-31 09:51:10 -0300

    Rotas e rotas child, queryParamsHandling preserve

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    // Agora preciso obter as informações passadas no path
+    const id = +this.route.snapshot.params['id']; //Pega imagem do path
+    this.server = this.serversService.getServer(id); // Pego o server com id
+    //Aqui eu faço o subscribe, o update com o que vem do path, obtenho o Server a partir do id que vem do path
+    this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
   }
 
+  onEdit() {
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
+    });
+  }
 }

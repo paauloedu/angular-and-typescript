6c65a2dd0358aa99bea1b06eb64a4e646404d526
Paulo Eduardo <pauloinfba@gmail.com>
2023-04-10 11:30:02 -0300

    Inicio de formulários

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f') singupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+  onSubmit() {
+    console.log(this.singupForm);
   }
 }

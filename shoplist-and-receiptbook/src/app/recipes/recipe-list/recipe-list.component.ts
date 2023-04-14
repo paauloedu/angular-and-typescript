8c2068613b71d578d0ee03b856e1f64229cf8905
Paulo Eduardo <pauloinfba@gmail.com>
2023-04-14 11:08:50 -0300

    Criando formulario Reativo no projeto

--- shoplist-and-receiptbook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shoplist-and-receiptbook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(
     private recipeService: RecipeService,
@@ -18,7 +20,16 @@ export class RecipeListComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.subscription = this.recipeService.recipesChanged.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }
+    );
     this.recipes = this.recipeService.getRecipes(); //Sempre que iniciar, obtem a lista de receitas que está no service e armazena nos recipes
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onNewRecipe() {

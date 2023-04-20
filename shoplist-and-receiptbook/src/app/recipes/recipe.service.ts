03e09ad8af3085c95daade67caae0a1454988fa4
Paulo Eduardo <pauloinfba@gmail.com>
2023-04-20 10:24:29 -0300

    Implementando conhecimentos Http ao projeto, Resolvers

--- shoplist-and-receiptbook/src/app/recipes/recipe.service.ts
+++ shoplist-and-receiptbook/src/app/recipes/recipe.service.ts
@@ -8,33 +8,40 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Torta de Pizza',
-      'Uma deliciosa torta com ingredientes e formato de pizza',
-      'https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg',
-      [
-        new Ingredient('Tomate', 2),
-        new Ingredient('Cebola', 2),
-        new Ingredient('Calabresa', 1),
-        new Ingredient('Queijo', 1),
-      ]
-    ),
-    new Recipe(
-      'Bolinho de queijo',
-      'Um perfeito bolinho de queijo com alguns molhos',
-      'https://www.indianhealthyrecipes.com/wp-content/uploads/2016/09/breakfast-recipes-fb.jpg',
-      [
-        new Ingredient('Farinha de Trigo', 1),
-        new Ingredient('Tomate', 1),
-        new Ingredient('Tempero', 3),
-        new Ingredient('Cebola', 1),
-        new Ingredient('Queijo', 4),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Torta de Pizza',
+  //     'Uma deliciosa torta com ingredientes e formato de pizza',
+  //     'https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg',
+  //     [
+  //       new Ingredient('Tomate', 2),
+  //       new Ingredient('Cebola', 2),
+  //       new Ingredient('Calabresa', 1),
+  //       new Ingredient('Queijo', 1),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Bolinho de queijo',
+  //     'Um perfeito bolinho de queijo com alguns molhos',
+  //     'https://www.indianhealthyrecipes.com/wp-content/uploads/2016/09/breakfast-recipes-fb.jpg',
+  //     [
+  //       new Ingredient('Farinha de Trigo', 1),
+  //       new Ingredient('Tomate', 1),
+  //       new Ingredient('Tempero', 3),
+  //       new Ingredient('Cebola', 1),
+  //       new Ingredient('Queijo', 4),
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();

695f0fc74dfd0fc028d658457a906ea2fb4e81c5
Paulo Eduardo <pauloinfba@gmail.com>
2023-04-25 08:10:35 -0300

    Adicionando parte de Autenticação feita

--- shoplist-and-receiptbook/src/app/app-routing.module.ts
+++ shoplist-and-receiptbook/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
@@ -28,6 +29,7 @@ const appRoutes: Routes = [
     ],
   },
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({

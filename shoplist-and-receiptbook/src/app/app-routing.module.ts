ae2dd4bfc39a4b5e8855f3a51055776de3a614fb
Paulo Eduardo <pauloinfba@gmail.com>
2023-04-26 10:54:19 -0300

    [ERRO - Não carregando] Começando Lazy Load

--- shoplist-and-receiptbook/src/app/app-routing.module.ts
+++ shoplist-and-receiptbook/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    loadChildren: () =>
+      import('./recipes/recipes.module').then((m) => m.RecipesModule),
+  },
 ];
 
 @NgModule({
